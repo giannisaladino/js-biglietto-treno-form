0fc049f9fa4fa3fc9be3d2d9194ec15b51e8b53e
giannisaladino <saladinogianni6@gmail.com>
2024-03-05 16:45:42 +0100

    ADD: stampa degli otuput su html

--- js/app.js
+++ js/app.js
@@ -12,44 +12,46 @@ console.log(selectAgeElement);
 const submitElement = document.getElementById('submit');
 console.log(submitElement);
 
+const credenziali = document.getElementById('credenziali');
+const risultato = document.getElementById('risultato');
 
 // aggiungo il listener del clic sul pulsante
 submitElement.addEventListener('click', function () {
 
-    name = inputNameElement.value
-    distanza = inputDistanceElement.value 
-    eta = selectAgeElement.value
+    name = inputNameElement.value; // string
+    distanza = parseFloat(inputDistanceElement.value); // float
+    eta = selectAgeElement.value; // string
 
- // 3 Calcolare il prezzo totale del viaggio in base all'età del passeggero
-const prezzo = 0.21; //number
+    // 3 Calcolare il prezzo totale del viaggio in base all'età del passeggero
+    const prezzo = 0.21; //number
 
-let total = distanza * 0.21; //number
+    let total = distanza * 0.21; // float
 
-// SE il passeggero è minorenne avrà uno sconto del 20%
+    // SE il passeggero è minorenne avrà uno sconto del 20%
 
-let sconto;
+    let sconto;
 
-if(eta === 'minorenne') {
-    console.log(name);
-    sconto = total * 0.20;
-    total = total - sconto;
-    console.log(total.toFixed(2));
-}
+    if (eta === 'minorenne') {
+        credenziali.innerHTML = name;
+        sconto = total * 0.20;
+        total = total - sconto;
+        risultato.innerHTML = 'Il costo del biglietto è: ' + total.toFixed(2);
+    }
 
-// ALTRIMENTI SE il passeggero è un over 65 avrà uno sconto del 40%
+    // ALTRIMENTI SE il passeggero è un over 65 avrà uno sconto del 40%
 
- else if(eta === 'over') {
-    console.log(name);
-    sconto = total * 0.40;
-    total = total - sconto;
-    console.log(total.toFixed(2));
- }
+    else if (eta === 'over') {
+        credenziali.innerHTML = name;
+        sconto = total * 0.40;
+        total = total - sconto;
+        risultato.innerHTML = 'Il costo del biglietto è: ' + total.toFixed(2);
+    }
 
-// ALTRIMENTI stampa il prezzo fisso
+    // ALTRIMENTI stampa il prezzo fisso
 
- else {
-    console.log(name);
-    console.log(total.toFixed(2));
- }
+    else {
+        credenziali.innerHTML = name;
+        risultato.innerHTML = 'Il costo del biglietto è: ' + total.toFixed(2);
+    }
 
 })
